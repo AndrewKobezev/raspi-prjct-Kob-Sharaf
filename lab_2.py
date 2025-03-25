cd392109e52c111f262751d1e79db98650e3a2da
KobezevA <Andrey.Kobezev@nkt.ru>
2025-03-25 17:09:42 +0300

    Ввел тестовый файлик + сделал дз

--- lab_2.py
+++ lab_2.py
@@ -1,48 +1,136 @@
-def get_op():
+import math
+
+# Функция выбора режима работы калькулятора
+def choose_regime():
+    print('\nВыбирите режим калькулятора:\n\t1)Решение линейного уравнения\n\t2)Решение квадратного уравнения\n\t3)Режим калькулятора\n')
     while True:
-        operat = input('введите операцию\n')
-        if operat in ['-','/','*','+']:
-            return operat
-            break
-        
+        regime = input()
+        if regime in ['1', '2', '3']:
+            regime = int(regime)
+            return regime
         else:
-            pass  
-    
-def get_num():
-    
-    while True:
-        num_1 = input('введите 1\n')
-        num_2 = input('введите 2\n')
-       
-        try:
-            
-            num_1 = float(num_1)
-            num_2 = float(num_2)
-                        
-            return num_1, num_2
-            break
-        
-        except:
-            print ('ВВОДИ ЧИСЛА!!!')
-       
-            
-def calc(op, num1, num2):
+            print('ВВЕДИТЕ 1 ИЛИ 2 ИЛИ 3 !!!!\n')
+            pass
+
+# Функция блока калькулятора
+def calc():
+
+    # Функция получения операции вычисления
+    def get_op():
+        while True:
+            operator = input('введите операцию\n')
+            if operator in ['-', '/', '*', '+']:
+
+                return operator
+            else:
+                pass
+
+    # Функция получения операндов
+    def get_num():
+        while True:
+            num_1 = input('введите 1\n')
+            num_2 = input('введите 2\n')
+
+            try:
+                num_1 = float(num_1)
+                num_2 = float(num_2)
+
+                return num_1, num_2
+            except:
+                print('ВВОДИ ЧИСЛА!!!')
+
+    # Запись переменных с операцией и операндами
+    op = get_op()
+    num1, num2 = get_num()
+
+    # Проверка деления на ноль
     if op == '/':
         while num2 == 0:
             num2 = input('ДЕЛЕНИЕ НА НОЛЬ. ВВЕДИТЕ ЕЩЁ РАЗ!\n')
             num2 = float(num2)
     else:
         pass
-        
-    arg = 'x'+op+'y'
-    f = lambda x,y: eval(arg)
+
+    # Блок непосредственного вычисления
+    arg = 'x' + op + 'y'
+    f = lambda x, y: eval(arg)
     print(f'{num1} {op} {num2} = ', f(num1, num2))
-        
-            
-       
-op = get_op()
-num1, num2 = get_num()
+
+# Функция блока линейного уравнения
+def lin_eq():
+    print('Форма уравнения: y = k*x + b')
+    while True:
+        k = input('Введите k\n')
+        b = input('Введите b\n')
+        y = input('Введите y\n')
+
+        try:
+            k = float(k)
+            b = float(b)
+            y = float(y)
+
+            while k == 0:
+                k = input('k не должно быть равно 0! Введите ещё раз\n')
+                k = float(k)
+
+            answr = (y - b) / k
+            print('Ответ: х =', answr)
+
+            break
+        except:
+            print('ВВОДИ ЧИСЛА!!!')
 
 
+# Функция блока квадратного уравнения
+def sq_eq():
+    print('Форма уравнения: y = a*x^2 + b*x + c')
+    while True:
+        a = input('Введите a\n')
+        b = input('Введите b\n')
+        c = input('Введите c\n')
+        y = input('Введите y\n')
 
-calc(op, num1, num2)
+        try:
+            a = float(a)
+            b = float(b)
+            c = float(c)
+            y = float(y)
+
+            c = c - y
+            discr = b ** 2 - 4 * a * c
+
+            if discr > 0:
+                x1 = (-b + math.sqrt(discr)) / (2 * a)
+                x2 = (-b - math.sqrt(discr)) / (2 * a)
+                print("Ответ: x_1 = %.6f \n\t   x_2 = %.6f" % (x1, x2))
+
+            elif discr == 0:
+                x = -b / (2 * a)
+                print("x = %.6f" % x)
+            else:
+                print("Корней нет")
+
+            break
+        except:
+            print('ПРОВЕРЬТЕ ВВЕДЁННЫЕ ЗНАЧЕНИЯ!!!')
+
+
+# Главная функция программы - выбор между режимами и вызов соответствующих функций
+def overall_calculator():
+    choose_reg = choose_regime()
+
+    # Условие выбора блока линейного уравнения
+    if choose_reg == 1:
+        lin_eq()
+
+    # Условие выбора блока квадратного уравнения
+    elif choose_reg == 2:
+        sq_eq()
+
+    # Условие выбора блока калькулятора
+    elif choose_reg == 3:
+        calc()
+
+# Безконечный вызов главной функции
+while True:
+    overall_calculator()
